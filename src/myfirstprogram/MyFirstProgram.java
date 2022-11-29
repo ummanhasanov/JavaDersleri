117341d1a5a07f9eb8a721446c3370574b8a799f
Umman Hasanov <88348414+ummanhasanov@users.noreply.github.com>
2022-11-29 08:03:38 +0400

    first program in JAVA

---
+++ src/myfirstprogram/MyFirstProgram.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirstprogram;
+
+/**
+ *
+ * @author Umman
+ */
+public class MyFirstProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        int x = 5;
+        int y = 10;
+        int z = x + y;
+        System.out.println(z);
+
+        int c = x - y;
+        System.out.println(c);
+
+        int v = x * y;
+        System.out.println(v);
+
+        double a = 18;
+        double d = 4;
+        double b = a / d;
+        System.out.println(b);
+
+        float f = (float) b;
+        System.out.println(f);
+
+        char s = 's';
+        System.out.println(s);
+
+        String soz = "Salam dunya";
+        System.out.println(soz);
+
+        Boolean cvb = true;
+        System.out.println(cvb);
+
+        long l = 5;
+        System.out.println(l);
+        
+        int a1 = 5;
+        int b1 = 2;
+        System.out.println("c=" + (a1 + b1));
+
+        double bol = (double) a1 / b1;
+        System.out.println(bol);
+    }
+
+}
